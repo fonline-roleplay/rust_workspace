70f7887e011a04128e2f5c841c32f305305da45b
qthree <qthree3@gmail.com>
2019-06-13 17:23:14 +0700

    Initial avatars support

--- client_dll/src/lib.rs
+++ client_dll/src/lib.rs
@@ -1,8 +1,12 @@
 #![cfg(windows)]
 
-use tnf_common::{engine_types::ScriptString};
+use tnf_common::{
+    engine_types::{
+        ScriptString, ScriptArray,
+    }
+};
 
-#[cfg(debug_assertions)]
+//#[cfg(debug_assertions)]
 tnf_common::dll_main!({});
 
 #[no_mangle]
@@ -17,4 +21,23 @@ pub extern "C" fn open_link(link: &ScriptString) {
     } else {
         println!("Invalid link: {:?}", link);
     }
+}
+
+fn send_avatars(avatars: Vec<u8>) -> std::io::Result<()> {
+    use std::io::Write;
+    let mut stream = std::net::TcpStream::connect_timeout(&"127.0.0.1:33741".parse().unwrap(), std::time::Duration::from_micros(50))?;
+    stream.write_all(&avatars[..])
+}
+
+#[no_mangle]
+pub extern "C" fn update_avatars(array: &ScriptArray) {
+    //let points: Option<&[i32]> = array.cast();
+    let buffer = array.buffer();
+    //if let Some(points) = points {
+        //let vec = points.to_owned();
+        let vec = buffer.to_owned();
+        std::thread::spawn(move || {
+            send_avatars(vec);
+        });
+    //}
 }

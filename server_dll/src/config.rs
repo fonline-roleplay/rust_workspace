4066580acd1a5df8e75820c9323318efa29d9884
qthree <qthree3@gmail.com>
2019-10-12 21:05:22 +0700

    Fully configurable check look; config hot reload

--- server_dll/src/config.rs
+++ server_dll/src/config.rs
@@ -1,64 +1,192 @@
+use arc_swap::ArcSwap;
 use derivative::Derivative;
-use once_cell::sync::OnceCell;
+use once_cell::sync::Lazy;
 use serde::Deserialize;
+use std::{ops::Deref, sync::Arc};
 
-const MAP_UTILITY_START: u16 = 92;
-const DEFAULT_FAST_FROM: u16 = 2200;
-const DEFAULT_FAST_TO: u16 = u16::max_value();
+mod defaults {
+    pub const MAP_UTILITY_START: u16 = 92;
+    pub const NPC_FAST_FROM: u16 = 2200;
+    pub const NPC_FAST_TO: u16 = u16::max_value();
+
+    pub const DEFAULT_BONUS: u32 = 10;
+    pub const DEFAULT_PERCEPTION: u32 = 5;
+
+    pub const VIEW_BONUS: u32 = 10;
+    pub const VIEW_PERCEPTION: u32 = 5;
+
+    pub const HEAR_BONUS: u32 = 5;
+    pub const HEAR_BONUS_NPC: u32 = 25;
+    pub const HEAR_PERCEPTION: u32 = 2;
+
+    pub const DIR_RATE_DEFAULT: [f32; 4] = [1.0; 4];
+    //                                    0    1    2    3
+    pub const DIR_RATE_VIEW: [f32; 4] = [1.0, 0.8, 0.5, 0.4];
+    pub const DIR_RATE_HEAR: [f32; 4] = [0.8, 1.0, 0.8, 0.8];
+
+    pub const MOVING_DEFAULT: f32 = 1.0;
+    pub const MOVING_SELF_RUN: f32 = 0.8;
+    pub const MOVING_TARGET_RUN: f32 = 3.0;
+
+    pub const WALL_RATE_DEFAULT: [f32; 10] = [0.0; 10];
+    pub const WALL_RATE_HEAR: [f32; 10] = [
+        0.1, 0.1, 0.1, 0.1, // 1..=4
+        0.3, 0.3, 0.3, 0.3, // 5..=8
+        0.4, 0.4, // 9..=10
+    ];
+}
+use defaults::*;
 
 #[derive(Deserialize, Default)]
+#[serde(default)]
 pub struct ServerConfig {
     pub check_look: CheckLook,
 }
 
 #[derive(Deserialize, Derivative)]
 #[derivative(Default)]
+#[serde(default)]
 pub struct CheckLook {
-    pub npc: CheckLookNpc,
+    pub npc_fast: NpcFast,
+    #[derivative(Default(value = "vec![SenseRates::default_view(), SenseRates::default_hear()]"))]
+    pub senses: Vec<SenseRates>,
     #[derivative(Default(value = "MAP_UTILITY_START"))]
     pub map_utility_start: u16,
 }
 
 #[derive(Deserialize, Derivative)]
 #[derivative(Default)]
-pub struct CheckLookNpc {
+#[serde(default)]
+pub struct SenseRates {
+    pub player: CritterRates,
+    pub npc: CritterRates,
+    #[derivative(Default(value = "DIR_RATE_DEFAULT"))]
+    pub dir_rate: [f32; 4],
+    pub self_moving: MovingRates,
+    pub target_moving: MovingRates,
+    #[derivative(Default(value = "WALL_RATE_DEFAULT"))]
+    pub wall_rate: [f32; 10],
+}
+impl SenseRates {
+    fn default_view() -> Self {
+        Self {
+            dir_rate: DIR_RATE_VIEW,
+            ..Default::default()
+        }
+    }
+    fn default_hear() -> Self {
+        Self {
+            player: CritterRates::default_hear_player(),
+            npc: CritterRates::default_hear_npc(),
+            dir_rate: DIR_RATE_HEAR,
+            self_moving: MovingRates::default_hear_self(),
+            target_moving: MovingRates::default_hear_target(),
+            wall_rate: WALL_RATE_HEAR,
+        }
+    }
+}
+
+/*
+#[derivative(Default(value = "VIEW_BONUS"))]
+    pub view_bonus: u32,
+    #[derivative(Default(value = "VIEW_PERCEPTION"))]
+    pub view_perception: u32,
+    #[derivative(Default(value = "HEAR_BONUS"))]
+    pub hear_bonus: u32,
+    #[derivative(Default(value = "HEAR_PERCEPTION"))]
+    pub hear_perception: u32,
+    */
+
+#[derive(Deserialize, Derivative)]
+#[derivative(Default)]
+#[serde(default)]
+pub struct NpcFast {
     #[derivative(Default(value = "true"))]
-    pub fast: bool,
-    #[derivative(Default(value = "CheckLookNpc::default_fast_from()"))]
-    #[serde(default = "CheckLookNpc::default_fast_from")]
+    pub enable: bool,
+    #[derivative(Default(value = "0"))]
+    pub sense_index: usize,
+    #[derivative(Default(value = "NPC_FAST_FROM"))]
     pub fast_from: u16,
-    #[derivative(Default(value = "CheckLookNpc::default_fast_to()"))]
-    #[serde(default = "CheckLookNpc::default_fast_to")]
+    #[derivative(Default(value = "NPC_FAST_TO"))]
     pub fast_to: u16,
 }
-impl CheckLookNpc {
-    const fn default_fast_from() -> u16 {
-        DEFAULT_FAST_FROM
+
+#[derive(Deserialize, Derivative)]
+#[derivative(Default)]
+pub struct CritterRates {
+    #[derivative(Default(value = "DEFAULT_BONUS"))]
+    pub basic_bonus: u32,
+    #[derivative(Default(value = "DEFAULT_PERCEPTION"))]
+    pub basic_perception_rate: u32,
+}
+impl CritterRates {
+    const fn default_hear_npc() -> Self {
+        Self {
+            basic_bonus: HEAR_BONUS_NPC,
+            basic_perception_rate: HEAR_PERCEPTION,
+        }
     }
-    const fn default_fast_to() -> u16 {
-        DEFAULT_FAST_TO
+    const fn default_hear_player() -> Self {
+        Self {
+            basic_bonus: HEAR_BONUS,
+            basic_perception_rate: HEAR_PERCEPTION,
+        }
     }
 }
 
-static CONFIG: OnceCell<ServerConfig> = OnceCell::new();
-
-pub fn config() -> &'static ServerConfig {
-    CONFIG.get_or_init(|| {
-        let res = load_config();
-        match res {
-            Ok(config) => config,
-            Err(err) => {
-                eprintln!("Error loading server config: {:?}", err);
-                Default::default()
-            }
+#[derive(Deserialize, Derivative)]
+#[derivative(Default)]
+pub struct MovingRates {
+    #[derivative(Default(value = "MOVING_DEFAULT"))]
+    pub still: f32,
+    #[derivative(Default(value = "MOVING_DEFAULT"))]
+    pub walking: f32,
+    #[derivative(Default(value = "MOVING_DEFAULT"))]
+    pub running: f32,
+}
+impl MovingRates {
+    fn default_hear_self() -> Self {
+        MovingRates {
+            running: MOVING_SELF_RUN,
+            ..Default::default()
         }
-    })
+    }
+    fn default_hear_target() -> Self {
+        MovingRates {
+            running: MOVING_TARGET_RUN,
+            ..Default::default()
+        }
+    }
+}
+
+static CONFIG: Lazy<ArcSwap<ServerConfig>> =
+    Lazy::new(|| ArcSwap::new(Arc::new(load_config_or_default())));
+
+pub fn config() -> impl Deref<Target = impl Deref<Target = ServerConfig>> {
+    CONFIG.load()
+}
+
+#[no_mangle]
+pub extern "C" fn reload_config() {
+    let new_config = load_config_or_default();
+    CONFIG.store(Arc::new(new_config));
 }
 
 #[derive(Debug)]
 enum ConfigError {
     Io(std::io::Error),
     Toml(toml::de::Error),
+}
+
+fn load_config_or_default() -> ServerConfig {
+    let res = load_config();
+    match res {
+        Ok(config) => config,
+        Err(err) => {
+            eprintln!("Error loading server config: {:?}", err);
+            Default::default()
+        }
+    }
 }
 
 fn load_config() -> Result<ServerConfig, ConfigError> {
--- server_dll/src/utils.rs
+++ server_dll/src/utils.rs
@@ -1,4 +1,4 @@
-use crate::config::config;
+use crate::config::{config, CheckLook, CritterRates, MovingRates, SenseRates};
 use tnf_common::{
     dll::param_getters,
     engine_types::{critter::Critter, map::Map},
@@ -122,9 +122,143 @@ pub extern "C" fn check_look(
     let cr = validate!(cr, false);
     let opponent = validate!(opponent, false);
 
-    let config = config();
+    let config = &config().check_look;
 
-    if map.proto_id() == config.check_look.map_utility_start
+    let smart = check_look_smart(config, map, cr, opponent);
+
+    /*let old = check_look_old(config, map, cr, opponent);
+    if old != smart {
+        println!("old != smart: {:?} != {:?}", old, smart);
+    }*/
+
+    /*let mut config_default = CheckLook::default();
+    config_default.npc_fast.enable = config.npc_fast.enable;
+    let smart_default = check_look_smart(&config_default, map, cr, opponent);
+
+    if smart != smart_default {
+        println!("smart != smart_default: {:?} != {:?}", smart, smart_default);
+    }*/
+
+    smart
+}
+
+fn check_look_smart(config: &CheckLook, map: &Map, cr: &Critter, opponent: &Critter) -> bool {
+    if map.proto_id() == config.map_utility_start
+        && opponent.is_player()
+        && cr.is_player()
+        && !cr.have_gm_vision()
+    {
+        return false;
+    }
+
+    let cr_hex = cr.hex();
+    let opp_hex = opponent.hex();
+    let dist = cr_hex.get_distance(opp_hex);
+
+    use tnf_common::defines::param::{CritterParam, Param};
+    let cr_vision = cr.uparam(Param::QST_VISION);
+
+    let opp_invis = opponent.uparam(Param::QST_INVIS);
+
+    if cr_vision >= dist && opp_invis <= dist {
+        return true;
+    }
+    if opp_invis != 0 && (opp_invis - 1) < dist {
+        // && ( !( cr.IsPlayer() ) || cr.IsPlayer() && !isGM( cr ) ) )
+        return false;
+    }
+    if opp_invis > dist || cr_vision >= dist {
+        return true;
+    }
+
+    let cr_perception = param_getters::getParam_Perception(cr, 0) as u32;
+    assert!(cr_perception >= 1 && cr_perception <= 10);
+
+    fn basic_dist(rates: &CritterRates, perception: u32) -> u32 {
+        rates.basic_bonus + perception * rates.basic_perception_rate
+    }
+
+    let self_is_npc = cr.is_npc();
+
+    if self_is_npc {
+        if cr.is_dead() {
+            return false;
+        }
+        let npc_fast = &config.npc_fast;
+        if npc_fast.enable && cr.ProtoId >= npc_fast.fast_from && cr.ProtoId <= npc_fast.fast_to {
+            return basic_dist(&config.senses[npc_fast.sense_index].npc, cr_perception) >= dist;
+        }
+    }
+
+    let start_dir = cr_hex.get_direction(opp_hex);
+    let mut look_dir = i8::abs(start_dir as i8 - cr.Dir as i8); //Направление
+
+    if look_dir > 3 {
+        look_dir = 6 - look_dir
+    }
+    assert!(look_dir >= 0 && look_dir <= 3);
+
+    fn moving_rate(cr: &Critter, rates: &MovingRates) -> f32 {
+        if cr.IsRuning {
+            rates.running
+        //} else if cr.is_walking() {
+        //    rates.walking
+        } else {
+            rates.still
+        }
+    }
+
+    fn sense_mul(rates: &SenseRates, cr: &Critter, opponent: &Critter, look_dir: i8) -> f32 {
+        rates.dir_rate[look_dir as usize]
+            * moving_rate(cr, &rates.self_moving)
+            * moving_rate(opponent, &rates.target_moving)
+    }
+
+    let senses: Vec<(f32, f32)> = config
+        .senses
+        .iter()
+        .map(|sense| {
+            let critter_rates = if self_is_npc {
+                &sense.npc
+            } else {
+                &sense.player
+            };
+            let basic_dist = basic_dist(critter_rates, cr_perception);
+            let sense_mul = sense_mul(sense, cr, opponent, look_dir);
+            let wall_mul = sense.wall_rate[cr_perception as usize - 1];
+            let clear_dist = basic_dist as f32 * sense_mul;
+            //dbg!(clear_dist, wall_mul);
+            (clear_dist, wall_mul)
+        })
+        .collect();
+
+    let max_dist = senses
+        .iter()
+        .map(|(dist, _wall_mul)| *dist as u32)
+        .max()
+        .expect("At least one sense");
+
+    //dbg!(dist, max_dist);
+    if dist > max_dist {
+        return false;
+    }
+
+    let end_hex = get_hex_in_path(map, cr_hex, opp_hex, 0.0, dist);
+    if dist > cr_hex.get_distance(end_hex) {
+        for (basic_dist, wall_mull) in senses {
+            //dbg!(basic_dist * wall_mull, dist);
+            if (basic_dist * wall_mull) as u32 >= dist {
+                return true;
+            }
+        }
+        false
+    } else {
+        true
+    }
+}
+
+fn _check_look_old(config: &CheckLook, map: &Map, cr: &Critter, opponent: &Critter) -> bool {
+    if map.proto_id() == config.map_utility_start
         && opponent.is_player()
         && cr.is_player()
         && !cr.have_gm_vision()
@@ -159,8 +293,8 @@ pub extern "C" fn check_look(
         if cr.is_dead() {
             return false;
         }
-        let cfg_npc = &config.check_look.npc;
-        if cfg_npc.fast && cr.ProtoId >= cfg_npc.fast_from && cr.ProtoId <= cfg_npc.fast_to {
+        let cfg_npc = &config.npc_fast;
+        if cfg_npc.enable && cr.ProtoId >= cfg_npc.fast_from && cr.ProtoId <= cfg_npc.fast_to {
             return (10 + cr_perception * 5) >= dist;
         }
     }
@@ -199,12 +333,14 @@ pub extern "C" fn check_look(
     let max_view = (max_view as f32 * view_mul) as u32;
     let tmp_max_hear = (max_hear as f32 * hear_mul) as u32;
 
+    //dbg!(dist, max_view, tmp_max_hear);
+
     // new optimization: return early if distance larger than max_view and max_hear
     if dist > max_view && dist > tmp_max_hear {
         return false;
     }
 
-    let end_hex = get_hex_in_path(map, cr_hex, opp_hex, 0.0, max_view);
+    let end_hex = get_hex_in_path(map, cr_hex, opp_hex, 0.0, dist);
     if dist > cr_hex.get_distance(end_hex) {
         is_view = false;
         hear_mul *= match cr_perception {
@@ -219,6 +355,7 @@ pub extern "C" fn check_look(
     }
 
     let max_hear = (max_hear as f32 * hear_mul) as u32;
+    //dbg!(max_hear);
     if dist > max_hear {
         is_hear = false;
     }
